031056ff862aace3d3c7a9e19e37f3909183fd2d
Xuanchen-Lin <143455820+Xuanchen-Lin@users.noreply.github.com>
2023-10-30 17:31:25 -0400

    Add files via upload

---
+++ js/site.js
@@ -0,0 +1,47 @@
+const namesOfThePlaces = {
+    california: [
+      gpsCoordinates: 36.7783° N, 119.4179° W,
+      weather: 'Sunny',
+]
+    arizona: [
+      gpsCoordinates: 34.0489° N, 111.0937° W
+      weather: 'Sunny',
+]
+    newMexico: [
+      gpsCoordinates: 34.9727° N, 105.0324° W
+      weather: 'Sunny',
+]
+
+    nevada: [
+      gpsCoordinates: 38.8026° N, 116.4194° W
+      weather: 'Sunny',
+]
+
+    utah: [
+      gpsCoordinates: 39.3210° N, 111.0937° W
+      weather: 'Sunny',
+
+    washington: [
+      gpsCoordinates: 47.7511° N, 120.7401° W
+      weather: 'Rainy',
+]
+
+
+let reasons = {
+   "To escape responsibilities",
+   "To escape someone",
+   "Seeking salvation",
+   "For a family time",
+   "To take photos",
+   "To ride bikes",
+   "Seeking inspirations",
+   "To experience the Beatnik life",
+}
+
+
+let modesOfTransport = {
+   "Campervan/RV/trailer",
+   "Car",
+   "Train",
+   "Airplane",
+}
